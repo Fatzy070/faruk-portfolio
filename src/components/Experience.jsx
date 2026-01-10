819df32f281d032706e0072b73516314a3d48f46
Ogunsola Faruk <ogunsolafaruk8@gmail.com>
2026-01-10 14:57:41 +0100

    education

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -5,17 +5,16 @@ const Experience = () => {
    <>
        <main className="py-10 px-6 md:px-16">
           <section id="experience" className="">
-      <h2 className="text-3xl font-bold mb-8 ">
-        Experience
+      <h2 className="text-3xl text-center font-bold mb-8 ">
+        Experience & Education
       </h2>
 
-     
-      <div className="relative pl-6 border-l-2 border-blue-500 dark:border-blue-400">
-        {/* Dot */}
+      <div className="grid grid-cols-2 gap-3">
+          <section>
+          <div className="relative pl-6 border-l-2 border-blue-500 dark:border-blue-400">
         <span className="absolute left-[-6px] top-2 w-3 h-3 rounded-full bg-blue-500 dark:bg-blue-400"></span>
 
-        {/* Role & Company */}
-        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+        <h3 className="text-lg font-bold text-gray-900 dark:text-white">
           Web Developer Intern @ SPTech
         </h3>
 
@@ -28,16 +27,13 @@ const Experience = () => {
         <p className="mt-2 text-gray-700 dark:text-gray-300">
           Assisted in mentoring new students on web development fundamentals, supported team growth by providing learning resources, and created visual aids to enhance student understanding.
         </p>
-      </div>
-    </section>
-
-
+      </div>      
       <div className="relative pl-6 border-l-2 border-blue-500 dark:border-blue-400 pt-4">
      
         <span className="absolute left-[-6px] top-6 w-3 h-3 rounded-full bg-blue-500 dark:bg-blue-400"></span>
 
      
-        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+        <h3 className="text-lg font-bold text-gray-900 dark:text-white">
           Backend Developer at Xerv
         </h3>
 
@@ -54,7 +50,34 @@ const Experience = () => {
     Collaborated closely with frontend developers to integrate REST APIs and streamline platform responsiveness.
      Utilized Firebase for authentication workflows and cloud data synchronization.
         </p>
+      </div>  
+      </section> 
+
+      <section>
+      <div className="relative pl-6 border-l-2 border-blue-500 dark:border-blue-400 pt-4">
+     
+        <span className="absolute left-[-6px] top-6 w-3 h-3 rounded-full bg-blue-500 dark:bg-blue-400"></span>
+
+     
+        <h3 className="text-lg font-bold text-gray-900 dark:text-white">
+           SP Tech and Solutions - 
+        </h3>
+
+       
+        <p className="text-sm text-gray-500 dark:text-gray-400">
+          2025
+        </p>
+
+       
+        <p className="mt-2 text-gray-700 dark:text-gray-300">
+        <span className="text-blue-500"> ⁛ </span>Completed a Diploma in Full-Stack Development, gaining hands-on experience with modern web technologies.
+        </p>
       </div>
+      </section>
+      </div>
+    </section>
+
+
        </main>
   
    </>
